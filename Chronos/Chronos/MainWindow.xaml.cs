87aacc9ff4d790fbc7d33db384bf98a22fbcc950
eglik <eglik@odin.vision>
2018-09-17 01:33:02 +0900

    feat: ScreenManager
    ScreenManager is control window size.

--- Chronos/Chronos/MainWindow.xaml.cs
+++ Chronos/Chronos/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Chronos;
 
 namespace Chronos
 {
@@ -23,6 +24,7 @@ namespace Chronos
         public MainWindow()
         {
             InitializeComponent();
+            ScreenManager.Initialize(this);
         }
     }
 }
